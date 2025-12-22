2bad87742b9a636709f6231b210db73239a6029d
harrison-ug <lqbb0133@leeds.ac.uk>
2025-12-22 15:47:14 +0000

    alt school week 4 assesment

---
+++ student tracker/index.js
@@ -0,0 +1,4 @@
+const name = document.getElementById('name');
+const age = document.getElementById('age');
+const addStudentBtn = document.getElementById('addStudentBtn');
+const studentList = document.getElementById('studentList');
